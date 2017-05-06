aebfd489487c78ad8ec43440f024f697cb6fbddc
Administrator <Administrator@192.168.1.14>
2017-05-06 22:38:08 +0530

    intial state of sales tool

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,20 @@
+package ca.demo.salestool;
+
+public class Main
+{
+	private static void displayGreeting()
+	{
+		System.out.println("welcome karan");
+		System.out.println("hello krishna");
+		System.out.println("hello");
+	}
+	
+	public static void main(String str[])
+	{
+		SalesData obj=new SalesData();
+		
+		displayGreeting();
+		
+	}
+
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,17 @@
+package ca.demo.salestool;
+
+public class SalesData 
+{
+	
+
+	int data[]={0,1,4};
+	
+	public void display()
+	{
+		System.out.println("implement data to display");
+		System.out.println("veer");
+		
+	}
+	
+
+}
